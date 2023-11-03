f328fb0d2f3ba03e0a7dfca6ab2678040c5a2737
Okwor Joshua <105440696+Josh565565@users.noreply.github.com>
2023-11-03 14:17:38 +0100

    Update settings.py

--- Backend/seethio_live/seethio_live/settings.py
+++ Backend/seethio_live/seethio_live/settings.py
@@ -134,8 +134,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "joshua.okwor.250076@unn.edu.ng"
-EMAIL_HOST_PASSWORD = "sorobocash565"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 DEFAULT_FROM_EMAIL = "TestSite Team <noreply@example.com>"
 
 # django_project/settings.py
